24398b123f55c2ff972476131ddb2d60bef7f632
n18020 <n18020@std.it-college.ac.jp>
2018-06-28 03:55:18 +0000

    0628test

--- 0628/062802.sh
+++ 0628/062802.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-if [ "$#" -eq 0 ]; then
-	echo Usage:062802.sh PATH
-	exit 1
-fi
-
-echo $1
+if [ $# -eq 0 ] ; then
+	find $1 -type f | xargs du -b | sort -n | tac | head -n 5
+		exit 1
+else
+	echo Usage:062802.sh
+	fi
